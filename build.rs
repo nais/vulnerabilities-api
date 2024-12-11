16cb1690b14567310108264e85e4a0f7f4ac633c
Tommy Trøen <tommy.troen@nav.no>
2024-12-11 13:53:37 +0100

    wip: add scaffolding
    Co-Authored-By: Youssef Bel Mekki <youssef.bel.mekki@nav.no>

---
+++ build.rs
@@ -0,0 +1,6 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .out_dir("src/proto")
+        .compile_protos(&["proto/helloworld/helloworld.proto"],&["proto"])?;
+    Ok(())
+}
---
+++ src/server.rs
@@ -0,0 +1,38 @@
+use tonic::{Request, Response, Status};
+use hello_world::greeter_server::{Greeter, GreeterServer};
+use hello_world::{HelloReply, HelloRequest};
+
+pub mod hello_world {
+    tonic::include_proto!("helloworld"); // The string specified here must match the proto package name
+}
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(&self, request: Request<HelloRequest>) -> Result<Response<HelloReply>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = HelloReply {
+            message: format!("Hello {}!", request.into_inner().clone().name), // We must use .into_inner() as the fields of gRPC requests and responses are private
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>  {
+
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    use tonic::transport::Server;
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
