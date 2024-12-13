9462e31bbc47181b85c923b2c79d10dd4a90900a
Youssef Bel Mekki <38552193+ybelMekk@users.noreply.github.com>
2024-12-13 11:17:44 +0100

    add(server): counter and admin

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,10 @@
 use hello_world::greeter_server::{Greeter, GreeterServer};
 use hello_world::{HelloReply, HelloRequest};
+use hello_world::admin_server::{Admin, AdminServer};
+use hello_world::{StatCountReply, StatCountRequest};
 use tonic::transport::Server;
 use tonic::{Request, Response, Status};
+use tonic::metadata::MetadataValue;
 
 pub mod hello_world {
     tonic::include_proto!("helloworld");
@@ -10,8 +13,41 @@ pub mod hello_world {
         tonic::include_file_descriptor_set!("helloworld_descriptor");
 }
 
+// This function will check the authorization header
+fn check_auth(req: Request<()>) -> Result<Request<()>, Status> {
+    let token: MetadataValue<_> = "Bearer my-token".parse().unwrap();
+    match  req.metadata().get("authorization") {
+        Some(t) if t == token => Ok(req),
+        _ => Err(Status::unauthenticated("Invalid token")),
+    }
+}
+
+// This is a type alias for the state of the server, which is a shared counter
+type State = std::sync::Arc<tokio::sync::RwLock<u64>>;
+
 #[derive(Debug)]
-pub struct MyGreeter {}
+pub struct AdminService {
+    stat: State,
+}
+
+// Implement the Admin trait for the AdminService struct
+#[tonic::async_trait]
+impl Admin for AdminService {
+    async fn get_stat_count(
+        &self,
+        _request: Request<StatCountRequest>,
+    ) -> Result<Response<StatCountReply>, Status> {
+        let stat = self.stat.read().await;
+        Ok(Response::new(StatCountReply {
+            count: *stat,
+        }))
+    }
+}
+
+#[derive(Debug)]
+pub struct MyGreeter {
+    state: State,
+}
 
 #[tonic::async_trait]
 impl Greeter for MyGreeter {
@@ -20,6 +56,13 @@ impl Greeter for MyGreeter {
         request: Request<HelloRequest>,
     ) -> Result<Response<HelloReply>, Status> {
         println!("Got a request: {:?}", request);
+        let input = request.get_ref();
+        self.increment_stat().await;
+
+        // Error handling, if the name field is empty, return an error
+        if input.name.is_empty() {
+            return Err(Status::invalid_argument("name must not be empty"));
+        }
 
         let reply = HelloReply {
             message: format!("Hello {}!", request.into_inner().clone().name), // We must use .into_inner() as the fields of gRPC requests and responses are private
@@ -30,8 +73,18 @@ impl Greeter for MyGreeter {
 }
 
 impl MyGreeter {
+    pub async fn increment_stat(&self) {
+        let mut stat = self.state.write().await;
+        *stat += 1;
+        println!("Stat counter: {}", *stat);
+    }
+}
+
+impl MyGreeter {
     pub fn new() -> MyGreeter {
-        MyGreeter {}
+        MyGreeter {
+            state: std::sync::Arc::new(tokio::sync::RwLock::new(0)),
+        }
     }
 }
 
@@ -44,7 +97,14 @@ impl Default for MyGreeter {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    let greeter = MyGreeter::new();
+    let state = State::default();
+    let greeter = MyGreeter {
+        state: state.clone(),
+    };
+
+    let admin = AdminService {
+        stat: state.clone(),
+    };
 
     let service = tonic_reflection::server::Builder::configure()
         .register_encoded_file_descriptor_set(hello_world::FILE_DESCRIPTOR_SET)
@@ -53,6 +113,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Server::builder()
         .add_service(service)
         .add_service(GreeterServer::new(greeter))
+        .add_service(AdminServer::with_interceptor(admin, check_auth))
         .serve(addr)
         .await?;
 
